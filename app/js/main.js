3160b17a6838eab84635e2991822698f83036e89
calvintan <calvin_tan@live.com>
2018-01-03 23:25:40 +0800

    insert scrollmagic to trigger animations

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
-console.log('test');
+(function ($) {
+
+// Init ScrollMagic
+var controller = new ScrollMagic.Controller();
+
+// loop through each .project element
+$('.container-inner').each(function(){
+  // build a scene
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.9,
+    reverse: false,
+  })
+  .setClassToggle(this, 'is-active') // add transition class
+  .addIndicators({
+    name: 'test'
+  }) // requires indicators plugin
+  .addTo(controller);
+})
+
+}(jQuery));
