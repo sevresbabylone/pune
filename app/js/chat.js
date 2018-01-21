73183e806e8ac5e8b99321b42161daf436f3d88e
sevresbabylone <cheryl.francais@gmail.com>
2018-01-21 18:29:30 +0800

    adds comments for chat.js

--- app/js/chat.js
+++ app/js/chat.js
@@ -2,7 +2,7 @@
 // @ document.ready
 // => trigger friend request animation,
 // => reduce height and scroll upwards
-// => add display: block to all the display:none sections
+// => add display:block to all the display:none sections
 // => first chat container appears
 // => add scrollMagic triggers for the other containers
 
@@ -10,13 +10,18 @@
     var leftChatContainers = $.grep($('.chat_container'), function(element) {
       return !$(element).hasClass('c-right')
     })
-
+    // adds click listener to accept-button
     $('#accept-button').click(function () {
+      // reduce height of story header
       $('.story_header').addClass('auto-height')
       $('#accept-button').off('click')
+      // add display: block to all the display:none sections
       $('.story_dialogue, .story_snippet, .story_quote, .story_album').css('display', 'block')
+      // first chat container appears
       $('.chat_text_first').addClass('animated fadeInLeft')
+      // init scrollMagic
       var controller = new ScrollMagic.Controller()
+      // adds scroll to left chat containers
       $(leftChatContainers).each(function (index, value) {
         $(value).addClass('animated')
         new ScrollMagic.Scene({
@@ -29,6 +34,7 @@
         }) // requires indicators plugin
         .addTo(controller)
       })
+      // adds scroll to right chat containers
       $('.c-right').each(function (index, value) {
         $(value).addClass('animated')
         new ScrollMagic.Scene({
@@ -41,6 +47,7 @@
         }) // requires indicators plugin
         .addTo(controller)
       })
+      // adds scroll to story quote containers
       $('.story_quote_container').each(function (index, value) {
         $(value).addClass('animated')
         new ScrollMagic.Scene({
@@ -53,6 +60,7 @@
         }) // requires indicators plugin
         .addTo(controller)
       })
+      // adds scroll to story snippet elements
       $('.story_snippet h3, .story_snippet img, .story_snippet p').each(function (index, value) {
         $(value).addClass('animated')
         new ScrollMagic.Scene({
@@ -65,6 +73,7 @@
         }) // requires indicators plugin
         .addTo(controller)
       })
+      // adds scroll to story photo
       $('.story_photo').each(function (index, value) {
         $(value).addClass('animated')
         new ScrollMagic.Scene({
